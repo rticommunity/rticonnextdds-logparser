4c4d0a3b39da6447a19bf8ae5ccc784940d5f034
Israel Blancas <iblancasa@gmail.com>
2017-01-30 10:41:49 +0100

    :sparkles: Add the basic structure to Micro and the error logs

---
+++ logparser/logs/micro/micro.py
@@ -0,0 +1,21 @@
+# Log Parser for RTI Connext.
+#
+#   Copyright 2016 Real-Time Innovations, Inc.
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+"""Log parsing functions for Micro."""
+
+from __future__ import absolute_import
+
+def on_micro_error_parsed(match, state, logger):
+    """An error from Micro was parsed"""
